e3debace0ea6b594be19c24a0c69418215322885
Hugo Oliveira Soares <hugoliveira.soares@gmail.com>
2021-11-14 19:48:14 -0300

    Algoritmo de kruskal funcionando

--- src/Grafos_Main.java
+++ src/Grafos_Main.java
@@ -6,12 +6,12 @@ public class Grafos_Main {
 	
 	public static void main(String[] args) {
 		
-		Grafos g1 = new Grafos("kruskal2.csv");
+		Grafos g1 = new Grafos("kruskal.csv");
 		
 //		System.out.println(g1.toString()); // Exibe info do grafo G
 		
 		// Outro modo de obter e exibir a info do grafo G
-//		System.out.println( "Matriz do grafo K5:\n" + g1.exibirMatrix(g1.getMatrizAdjacencias()) );
+		System.out.println( "Matriz do grafo:\n" + g1.exibirMatrix(g1.getMatrizAdjacencias()) );
 //		System.out.println( "Numero de vertices: " + g1.getNumVertices() );
 //		System.out.println( "Numero de arestas: " + g1.getNumArestas() );
 //		System.out.println( "Grau dos vertices: " + g1.exibirVetor(g1.getGrausVertices()) );
@@ -22,6 +22,12 @@ public class Grafos_Main {
 		//src.Grafos g3 = new src.Grafos("n4e5.csv");
 
 		Kruskal kruskal = new Kruskal(g1);
+
+		for (int i = 0; i < kruskal.getP().length; i++){
+			System.out.println(i + "," + kruskal.getP()[i]);
+		}
+
+//		System.out.println(g1.buscaProfundidade(2, 3));
 
 	}
 
--- src/kruskal/Kruskal.java
+++ src/kruskal/Kruskal.java
@@ -1,18 +1,15 @@
 package src.kruskal;
 
 import src.Grafos;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+
+import java.util.*;
 
 public class Kruskal {
 
     private Grafos grafo;
-    private LinkedList<Integer> r = new LinkedList<>();;
+    private LinkedList<Integer> r = new LinkedList<>();
     private List<int[]> arestas =  new ArrayList<>(); // lista de arestas identificadas pelos pares de vértices(int[])
     private int[] p; // Vetor de parents, de onde eu vim
-    private int[] t;
 
     public Kruskal() {
     }
@@ -27,28 +24,108 @@ public class Kruskal {
     public void geraArvoreMinima(int[][] matrizPesos){
 
         int vetorInicial = 0;
+        int anterior;
+
         p[vetorInicial] = 0; // Escolha um vértice Vi e marque-o com 0 na posição i em P
         r.add(vetorInicial);
+        anterior = r.getLast();
 
-        while (contains(p)) { // Faça enquanto p == -1
-            adjacentes(matrizPesos);
-            int[] vetices = menorAresta(matrizPesos); // obtenha a menor aresta
-            // De modo que não forme circuito
-            p[vetices[0]] = vetices[1]; // marque a posição j de p com o valor de i
+        adjacentes(matrizPesos, anterior);
+
+        while (containsmenos1P()) {// Faça enquanto p == -1
+            if (anterior != r.getLast() ) {
+                adjacentes(matrizPesos, r.getLast());
+                anterior = r.getLast();
+            }
+
+            int[] vertices = menorAresta(matrizPesos); // obtenha a menor aresta
+            if (!fechaCircuito(vertices[0], vertices[1])){
+                p[vertices[1]] = vertices[0]; // marque a posição j de p com o valor de i
+                r.add(vertices[1]);
+            }
+            arestas.remove(vertices);
         }
-
     }
 
-    private void adjacentes(int[][] matrizPesos) {
+    private void adjacentes(int[][] matrizPesos, int v) {
         for (int i = 0; i < matrizPesos.length; i++) {
-            if (matrizPesos[r.getLast()][i] != 0){
-                arestas.add(new int[]{r.getLast(), i});
+            if (matrizPesos[v][i] != 0){
+                if (!estaAdicionado(v, i) || !containsAresta(v, i)){
+                    arestas.add(new int[]{v, i});
+                }
             }
         }
     }
 
-    private boolean contains(int[] vetor) {
-        for (int x : vetor) {
+    public int[] menorAresta(int[][] matrizPesos){
+
+        // variaveis auxiliares para indentificação das arestas.
+        int vertice1 = arestas.get(0)[0], vertice2 = arestas.get(0)[1];
+
+        int vertices = 0;
+        int menor = matrizPesos[vertice1][vertice2]; // menor peso de aresta encontrado
+
+        for (int i = 1; i < arestas.size(); i++) {
+            vertice1 = arestas.get(i)[0];
+            vertice2 = arestas.get(i)[1];
+
+            if (matrizPesos[vertice1][vertice2] < menor){
+                menor = matrizPesos[vertice1][vertice2];
+                vertices = i;
+            }
+        }
+        return arestas.get(vertices);
+    }
+
+    public boolean fechaCircuito(int vertice, int buscado) {
+
+        int n = p.length;
+        boolean[] marcado = new boolean[n];
+        int[] antecessor = new int[n];
+        marcado[vertice] = true;
+
+        for (int w = 0; w < n; w++) {
+            if (eAdjacente(vertice, w) && !marcado[w]){
+                antecessor[w] = vertice;
+                DFS(w, marcado, antecessor, n);
+            }
+        }
+
+//        System.out.println(marcado[buscado]);
+        return marcado[buscado];
+    }
+
+    public void DFS(int vertice, boolean[] marcado, int[] antecessor, int n) {
+
+        Stack<Integer> pilha = new Stack<>(); // instanciar a pilha
+
+        marcado[vertice] = true;
+        pilha.push(vertice);// inserir na pilha
+
+        while(!pilha.isEmpty()) { // ver ser ta vazia
+            int w = pilha.pop();
+            for (int z = 0; z < n; z++) {
+                if (eAdjacente(w, z) && !marcado[z]) {
+                    antecessor[z] = w;
+                    marcado[z] = true;
+                    pilha.push(z);
+                }
+            }
+        }
+
+    }
+
+    private boolean eAdjacente(int v, int w) {
+        return p[v] == w || p[w] == v;
+    }
+
+    //Verifica se já está adicionado na arvore (no vetor p)
+    private boolean estaAdicionado(int x, int y) {
+        return p[x] == y || p[y] == x;
+    }
+
+    private boolean containsmenos1P() {
+        for (int x : p) {
             if (x == -1) {
                 return true;
             }
@@ -56,47 +133,18 @@ public class Kruskal {
         return false;
     }
 
-    public int[] menorAresta(int[][] matrizPesos){
+    private boolean containsAresta(int vertice1, int vertice2) {
 
-        // variaveis auxiliares para indentificação dos vertices.
-        int vertice1 = 0, vertice2 = 0;
-        int verticeX = arestas.get(0)[0], verticeY = arestas.get(0)[1];
-
-        int menor = matrizPesos[verticeX][verticeY]; // menor peso de aresta encontrado
-
-        for (int i = 1; i < arestas.size(); i++) {
-
-            verticeX = arestas.get(i)[0];
-            verticeY = arestas.get(i)[1];
-
-            if (matrizPesos[verticeX][verticeY] < menor){
-
-                menor = matrizPesos[arestas.get(i)[0]][arestas.get(i)[1]];
-                vertice1 = verticeX;
-                vertice2 = verticeY;
-            }
-        }
-        return new int[]{vertice1, vertice2};
-    }
-
-    private boolean fechaCircuito(int[][] matrizPesos, int[][] arvore, int vertice1, int vertice2){
-        int v1 = vertice1;
-        int v2 = vertice2;
-
-        for (int i = v2; i < matrizPesos.length; i++) {
-            for (int j = 0; j < matrizPesos.length; j++) {
-                if (j != v1){
-                    if (estaAdicionado(arvore[v2][j], matrizPesos[v2][j])){
-                        v2 = j;
-                    }
-                }
+        for (int[] vertices : arestas) {
+            if ((vertices[0] == vertice1 && vertices[1] == vertice2) ||
+                    (vertices[1] == vertice1 && vertices[0] == vertice2)) {
+                return true;
             }
         }
         return false;
     }
 
-    private boolean estaAdicionado(int x, int y) {
-        return x == y;
+    public int[] getP() {
+        return p;
     }
-
 }
